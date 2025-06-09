a3a806ac144cf170719d177add1fbe46415a2397
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2025-06-09 13:56:41 +0330

    ci: add coins logo domain to nextjs configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["cryptologos.cc"],
+  },
 };
 
 export default nextConfig;
