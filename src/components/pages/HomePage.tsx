ba23c9b2d1a71d9ed23919564941a9fed9648e77
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2025-06-11 01:53:07 +0330

    feat(ui): add two test spaces

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -4,7 +4,9 @@ import OpportunitiesSection from "../templates/OpportunitiesSection";
 function HomePage() {
   return (
     <div className="flex flex-col items-center">
+      <div className="h-[110vh]"></div>
       <OpportunitiesSection />
+      <div className="h-[120vh]"></div>
     </div>
   );
 }
