b43f8a462c0687b2f1216714c545f773dc98b42b
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2025-06-11 01:52:00 +0330

    feat(ui): add CardItem component implementation and add switch button

--- src/components/organism/Card.tsx
+++ src/components/organism/Card.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 import Image, { StaticImageData } from "next/image";
-
+import { shuffleArray } from "@/utils/helpers";
+import { AnimatePresence, motion } from "framer-motion";
+import CardItem from "./CardItem";
 interface coin {
   logo: string;
   name: string;
@@ -13,37 +17,43 @@ interface CardProps {
   coins: coin[];
   title: string;
   titleIcon: StaticImageData;
+  fetchData: boolean;
 }
 
-function Card({ coins, title, titleIcon }: CardProps): React.ReactElement {
+function Card({
+  coins,
+  title,
+  titleIcon,
+  fetchData,
+}: CardProps): React.ReactElement {
+  const [items, setItems] = useState(coins);
+  useEffect(() => {
+    if (fetchData === false) return; //using this state for stop our sample data fetching(works with viewPort)
+    const interval = setInterval(() => {
+      setItems((prev) => shuffleArray(prev));
+      console.log("change");
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
-    <div className="w-[330px] h-fit  flex flex-col gap-8 px-4 pt-5 pb-6 bg-[#13141A] border rounded-xl border-[#2B2C31]">
-      <div className="flex bg-[#101116] rounded-2xl w-fit pr-3 pl-2 py-0.5 items-center gap-1">
+    <div className="w-[330px] h-fit  flex flex-col gap-8  pt-5 pb-6 bg-[#13141A] border rounded-xl border-[#2B2C31] ">
+      <div className="flex bg-[#101116] rounded-2xl w-fit pr-3 pl-2 py-0.5 items-center gap-1 ml-4">
         <Image src={titleIcon} alt={title} width={17} height={17} />
         <span className="text-[#B3BBF8] text-[14px]">{title}</span>
       </div>
-      <div className="flex flex-col gap-4">
-        {coins.map((coin, index) => (
-          <div key={index} className="w-full h-fit flex justify-between">
-            <div className="flex gap-4 items-center">
-              <Image src={coin.logo} alt={coin.name} width={30} height={30} />
-              <p className="text-[14px]">
-                {coin.name}{" "}
-                <span className="text-[#5F6067] ">({coin.pair})</span>
-              </p>
-            </div>
-            <div className="flex flex-col items-end">
-              <p className="text-[14px]">{coin.price}</p>
-
-              {coin.change > 0 ? (
-                <span className="text-[#236A5C]">+{coin.change}%</span>
-              ) : (
-                <span className="text-[#922C43]">{coin.change}%</span>
-              )}
-            </div>
+      <motion.ul
+        className="flex flex-col gap-4 "
+        initial={{ y: "200px", opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: "0.4" }}
+      >
+        {items.map((coin, index) => (
+          <div key={index}>
+            <CardItem coin={coin} />
           </div>
         ))}
-      </div>
+      </motion.ul>
     </div>
   );
 }
---
+++ src/components/organism/CardItem.tsx
@@ -0,0 +1,56 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import sw from "@/../../public/icons/switch.svg";
+
+interface coin {
+  logo: string;
+  name: string;
+  pair: string;
+  price: number;
+  change: number;
+}
+interface CardItemProps {
+  coin: coin;
+}
+function CardItem({ coin }: CardItemProps) {
+  const [showDrag, setShowDrag] = useState(false);
+  return (
+    <motion.li
+      className="w-full h-fit flex justify-between relative hover:bg-[#0C0C14] px-4 "
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{ duration: 0.4, type: "spring", stiffness: 300 }}
+      onMouseEnter={() => setShowDrag(true)}
+      onMouseLeave={() => setShowDrag(false)}
+    >
+      {showDrag && (
+        <div
+          className="absolute size-[18px] right-[-8px] top-[14.5px] bg-[#7277F7]
+        rounded-[3px] flex items-center justify-center cursor-pointer "
+        >
+          <Image src={sw} alt="switch" width={14} height={14} />
+        </div>
+      )}
+      <div className="flex gap-4 items-center">
+        <Image src={coin.logo} alt={coin.name} width={30} height={30} />
+        <p className="text-[14px]">
+          {coin.name} <span className="text-[#5F6067] ">({coin.pair})</span>
+        </p>
+      </div>
+      <div className="flex flex-col items-end">
+        <p className="text-[14px]">{coin.price}</p>
+
+        {coin.change > 0 ? (
+          <span className="text-[#236A5C]">+{coin.change}%</span>
+        ) : (
+          <span className="text-[#922C43]">{coin.change}%</span>
+        )}
+      </div>
+    </motion.li>
+  );
+}
+
+export default CardItem;
